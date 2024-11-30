04fbf8292064be382c28bb7cb4b1675c4b046ac1
lenov <lenov@DESKTOP-NKN1LTV>
2024-11-30 12:14:41 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+void TrianglePS(float a, float& P, float& S) {
+    P = 3 * a;
+    S = (pow(a, 2) * sqrt(3)) / 4;
+}
+int main() {
+    setlocale(LC_ALL, "RU");
+    float number;
+    float res;
+    float res1;
+    for (int i = 0; i < 3; i++) {
+        cout << "Введите число" << endl;
+        cin >> number;
+        TrianglePS(number, res, res1);
+        cout << "Сторона: " << number << " " << "Периметр" << " " << res << " " << "Площадь" << " " << res1 << endl;
+    }
+
+
+    return 0;
+}
+
+
